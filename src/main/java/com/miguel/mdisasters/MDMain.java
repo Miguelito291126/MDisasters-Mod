e46a800dff25efe56f54dbc22847c078aaa9eba5
migue <migue@DESKTOP-7D53VLU>
2026-08-21 16:32:31 +0200

    fix version

--- src/main/java/com/miguel/mdisasters/MDMain.java
+++ src/main/java/com/miguel/mdisasters/MDMain.java
@@ -18,7 +18,7 @@ public class MDMain
 {
     public static final String MODID = "mdisasters";
     public static final String NAME = "MDisasters";
-    public static final String VERSION = "2.0.4";
+    public static final String VERSION = "2.0.3.6";
     public static final String CLIENT_PROXY_CLASS = "com.miguel.mdisasters.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.miguel.mdisasters.proxy.CommonProxy";
 
